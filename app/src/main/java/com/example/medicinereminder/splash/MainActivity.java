4cfc5f5c036bc500ae082bffd38ceda71c9cd298
Toba <mohamedtowba298@gmail.com>
2022-04-12 15:05:43 +0200

    set up

--- app/src/main/java/com/example/medicinereminder/splash/MainActivity.java
+++ app/src/main/java/com/example/medicinereminder/splash/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.medicinereminder;
+package com.example.medicinereminder.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.medicinereminder.R;
 
 public class MainActivity extends AppCompatActivity {
 
