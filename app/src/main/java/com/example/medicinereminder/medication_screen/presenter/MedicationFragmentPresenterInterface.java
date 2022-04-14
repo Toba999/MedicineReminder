7414c628cde0f33ecf4c55e599cee2515f072b67
Thoria hamdy <Thoriahamdy99@gmail.com>
2022-04-14 12:36:59 +0200

    Adding Medication screen UI

--- app/src/main/java/com/example/medicinereminder/medication_screen/presenter/MedicationFragmentPresenterInterface.java
+++ app/src/main/java/com/example/medicinereminder/medication_screen/presenter/MedicationFragmentPresenterInterface.java
@@ -1,6 +1,13 @@
 package com.example.medicinereminder.medication_screen.presenter;
 
+import com.example.medicinereminder.services.model.Medicine;
+
 import java.util.List;
 
 public interface MedicationFragmentPresenterInterface {
+    public List<String> getActiveInactive();
+    public List<Medicine> getMedicines();
+    public List<Medicine> getInactiveMedicines();
+    public List<Medicine> getActiveMedicines();
+    public void seperateMedicines();
 }
--- app/src/main/java/com/example/medicinereminder/splash/MainActivity.java
+++ app/src/main/java/com/example/medicinereminder/splash/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.medicinereminder.R;
+import com.example.medicinereminder.medication_screen.view.MedicationFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Bundle bundle = new Bundle();
+        bundle.putInt("some_int", 0);
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.medicationFragmentContainer, MedicationFragment.class, bundle)
+                .commit();
 
     }
 }
