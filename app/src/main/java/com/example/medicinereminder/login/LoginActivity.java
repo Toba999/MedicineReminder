adee4d3ee1756a142266934a2aa9e54fa901bf41
unknown <tasneim.hatem@gmail.com>
2022-04-14 21:26:31 +0200

    add UI Sign up screen

--- app/src/main/java/com/example/medicinereminder/login/LoginActivity.java
+++ app/src/main/java/com/example/medicinereminder/login/LoginActivity.java
@@ -16,7 +16,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        getSupportActionBar().hide();
         txtSign = findViewById(R.id.txtLogin);
         txtSign.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/medicinereminder/signup/SignUpActivity.java
+++ app/src/main/java/com/example/medicinereminder/signup/SignUpActivity.java
@@ -11,20 +11,19 @@ import com.example.medicinereminder.R;
 import com.example.medicinereminder.login.LoginActivity;
 
 public class SignUpActivity extends AppCompatActivity {
-    //TextView txtLogin;
+    TextView txtLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        getSupportActionBar().hide();
-//        txtLogin = findViewById(R.id.txtLogin);
-//        txtLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-//
-//            }
-//        });
+        txtLogin = findViewById(R.id.txtLogin);
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+
+            }
+        });
 
     }
 }
