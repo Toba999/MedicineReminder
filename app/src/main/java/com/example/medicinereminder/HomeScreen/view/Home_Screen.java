6d331f793cb2642e3006bddd727c541baaa00b1a
Mohamed Adel <67330386+MohamedAdel7710@users.noreply.github.com>
2022-04-14 11:48:39 +0200

    Add Navigation Bar

---
+++ app/src/main/java/com/example/medicinereminder/HomeScreen/view/Home_Screen.java
@@ -0,0 +1,66 @@
+package com.example.medicinereminder.HomeScreen.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.medicinereminder.HomeScreen.HomeFragment;
+import com.example.medicinereminder.HomeScreen.MedicationsFragment;
+import com.example.medicinereminder.HomeScreen.MoreFragment;
+import com.example.medicinereminder.R;
+import com.gauravk.bubblenavigation.BubbleNavigationLinearView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
+
+public class Home_Screen extends AppCompatActivity {
+
+    BubbleNavigationLinearView bNaviView;
+    FragmentTransaction fragmentTransaction;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        initUI();
+//        bNaviView.setBadgeValue(0,"" );
+//        bNaviView.setBadgeValue(1,"" );
+//        bNaviView.setBadgeValue(2,"" );
+
+        //set home first screen....
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.home_fragmentcontainer,new HomeFragment());
+        fragmentTransaction.commit();
+
+        bNaviView.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                if(position == 0)
+                {
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.home_fragmentcontainer,new HomeFragment());
+                    fragmentTransaction.commit();
+                }
+                else if(position == 1)
+                {
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.home_fragmentcontainer,new MedicationsFragment());
+                    fragmentTransaction.commit();
+                }
+                else{
+
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.home_fragmentcontainer,new MoreFragment());
+                    fragmentTransaction.commit();
+
+                }
+            }
+        });
+
+
+    }
+
+    void initUI()
+    {
+        bNaviView = findViewById(R.id.home_navigationBar);
+    }
+}
