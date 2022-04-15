8ab84c015b74f342c5527a22accb9cd0f7b1294c
unknown <tasneim.hatem@gmail.com>
2022-04-15 22:17:14 +0200

    adding Profile UI

--- app/src/main/java/com/example/medicinereminder/profile/ProfileActivity.java
+++ app/src/main/java/com/example/medicinereminder/profile/ProfileActivity.java
@@ -1,35 +1,25 @@
-package com.example.medicinereminder.addtaker;
+package com.example.medicinereminder.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.medicinereminder.R;
 
-public class AddTakerActivity extends AppCompatActivity {
-    Button btnAddTaker;
+public class ProfileActivity extends AppCompatActivity {
     ImageView exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_taker);
-        btnAddTaker = findViewById(R.id.btnAddTaker);
-        exit = findViewById(R.id.exitTaker);
+        setContentView(R.layout.activity_profile);
+        exit = findViewById(R.id.exitProfile);
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
-        btnAddTaker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
     }
 }
