a2bfd926b0c5f1ccd0311c0f51ed0786648a489d
mohamed toba <81019017+Toba999@users.noreply.github.com>
2022-04-16 13:49:19 +0200

    Revert "Toba"

--- app/src/main/java/com/example/medicinereminder/HomeScreen/view/Home_Screen.java
+++ app/src/main/java/com/example/medicinereminder/HomeScreen/view/Home_Screen.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.medicinereminder.HomeScreen.HomeFragment;
-
 import com.example.medicinereminder.more_screen.view.MoreFragment;
 import com.example.medicinereminder.R;
 import com.example.medicinereminder.medication_screen.view.MedicationsFragment;
