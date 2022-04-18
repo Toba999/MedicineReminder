fb369f9d26d6f13f064175be8ab38f4c7598ee84
Thoria hamdy <Thoriahamdy99@gmail.com>
2022-04-18 19:39:14 +0200

    Adding firebase

---
+++ app/src/main/java/com/example/medicinereminder/services/network/NetworkDelegate.java
@@ -0,0 +1,26 @@
+package com.example.medicinereminder.services.network;
+
+import com.example.medicinereminder.model.PatientDTO;
+import com.example.medicinereminder.model.RequestDTO;
+import com.example.medicinereminder.model.TrackerDTO;
+import com.example.medicinereminder.model.MedicationPOJO;
+
+import java.util.List;
+
+public interface NetworkDelegate {
+
+    void onSuccess();
+    void onFailure(String errorMessage);
+
+    // for response
+    void onSuccessReturn(String userName);
+    void onSuccessRequest(List<RequestDTO> requestDTOS);
+    void onSuccessTracker(List<TrackerDTO> trackerDTOS);
+    void onSuccess(boolean response);
+    void onSuccessPatient(List<PatientDTO> patientDTOS);
+    void isUserExist(boolean existance);
+    void onUpdateMedicationFromFirebase(List<MedicationPOJO> medications);
+
+    void onSuccessReturnMedicationList(List<MedicationPOJO> medicationPOJOList); //Medication
+
+}
