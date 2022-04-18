2c88adc23263f27d9aaefea3f861f84a29f9b3cd
unknown <tasneim.hatem@gmail.com>
2022-04-18 20:47:40 +0200

    adding Room DataBase

---
+++ app/src/main/java/com/example/medicinereminder/localdatabase/LocalSourceInterface.java
@@ -0,0 +1,28 @@
+package com.example.medicinereminder.localdatabase;
+
+
+
+import androidx.lifecycle.LiveData;
+
+import com.example.medicinereminder.model.MedicationPOJO;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+public interface LocalSourceInterface {
+    LiveData<List<MedicationPOJO>> getAllMedication();
+
+    void insertMedication(MedicationPOJO medication);
+
+    void deleteMedication(MedicationPOJO medication);
+    LiveData<MedicationPOJO> getMedications(int id);
+    void updateMedications(MedicationPOJO medicationPOJO);
+    LiveData<List<MedicationPOJO>> getActiveMedications(long time);
+    LiveData<List<MedicationPOJO>> getInactiveMedications(long time);
+
+    LiveData<List<MedicationPOJO>> getMedicationDay(long time);
+
+   // Single<List<MedicationPOJO>> getMedicationDayWorkManger(long time);
+  //  Single<List<MedicationPOJO>> getRefillReminderList(long time);
+}
